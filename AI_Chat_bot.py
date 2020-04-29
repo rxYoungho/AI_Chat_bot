a6d6a54c0173b9cfb204465072dc6b49dae5263c
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-04-29 14:59:44 +0900

    Update AI_Chat_bot.py

--- AI_Chat_bot.py
+++ AI_Chat_bot.py
@@ -1,11 +1,10 @@
 import nltk
-# nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy # dataset 관리
-# import tflearn 
-# import tensorflow 
+import numpy
+import tflearn
+import tensorflow
 import random
 import json
 
@@ -13,17 +12,50 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
+
+
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
-    
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
-# print(words)
 
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x): # cx keeps track of the iterator.
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+
+training = numpy.array(training)
+output = numpy.array(output)

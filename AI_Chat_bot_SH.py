a571fbc64e3efc8d1e53d97112377a13283ebb72
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-04-29 14:59:42 +0900

    Create AI_Chat_bot_SH.py

---
+++ AI_Chat_bot_SH.py
@@ -0,0 +1,30 @@
+import nltk
+# nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy # dataset 관리
+# import tflearn 
+# import tensorflow 
+import random
+import json
+
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs = []
+
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs.append(pattern)
+    
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+# print(words)
+
